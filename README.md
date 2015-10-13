7d3b9d60e4dc3f7a927f8c2ed4354b13b1a0c267
JoramWilander <jwawilander@gmail.com>
2015-10-13 17:41:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# GitLab to Mattermost Integration
+
+This integration makes use of GitLab's outgoing webhooks and Mattermost's incoming webhooks to post GitLab events into Mattermost. You can find GitLab's outgoing webhooks described [here](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md) and Mattermost's incoming webhooks described [here](https://github.com/mattermost/platform/blob/master/doc/integrations/webhooks/Incoming-Webhooks.md).
+
+## Installing
+
+This integration can be run from any web server that supports Python. The simplest way to get it up and running fast is to use Heroku.
+
+**Note: These installation instructions assume you have both a GitLab account with a project you have admin access to, and a Mattermost account where [incoming webhooks are enabled](https://github.com/mattermost/platform/blob/master/doc/integrations/webhooks/Incoming-Webhooks.md#enabling-incoming-webhooks).**
+
+
+### Quick Install with Heroku
+
+If you don't already have a Heroku account please go create one [here](https://www.heroku.com/) (it's free!).
+
+1. Deploy integration to Heroku
+ 1. Fork the [gitlab-mattermost](https://github.com/mattermost/gitlab-mattermost) repository on GitHub by clicking _Fork_ in the top-right and selecting your account if it asks.
+ 1. Log in to [Heroku](https://www.heroku.com/) and go to your [dashboard](https://dashboard.heroku.com/apps).
+ 2. Click the `+` in the top-right corner to add a new _app_.
+ 3. Give your app a name and select your region, then click _Create App_.
+ 4. On the _Deploy_ screen, select _GitHub_ at the top.
+ 5. Use _Connect to GitHub_ to authorize Herkou to access your GitHub account.
+ 6. Select your account and type `gitlab-mattermost` into the _repo-name_ field, then click _Search_.
+ 7. Click the _Connect_ button next to your repository.
+ 8. Scroll to the bottom of the new page and under the _Manual deploy_ section click _Deploy Branch_, making sure the `master` branch is selected.
+ 9. Go to the _Settings_ tab and under the _Domains_ section copy the _Heroku Domain_.
+
+2. Set up your GitLab outgoing webhook
+ 1. Log in to your GitLab account, and go to the project you want events pushed from.
+ 2. From your project page, click _Settings_ in the bottom-left and then click _Web Hooks_. Note that you need have admin access to the project.
+ 3. In the _URL_ field, enter the following `http://<your-heroku-domain>/new_event`. Make sure to replace `<your-heroku-domain>` with the domain you copied in the last step of the previous install section.
+ 4. Select all the _Triggers_ you want to be posted into Mattermost, then click _Add Web Hook_.
+
+3. Set up your Mattermost incoming webhook
+ 1. Log in to your Mattermost account, and open your _Account Settings_ by clicking in the top-left.
+ 2. Go to the _Integrations_ tab and click _Edit_ next to _Manage your incoming webhooks_.
+ 3. Select the channel you want the GitLab events to post to, then click _Add_.
+ 4. Copy the _URL_ from the newly created webhook.
+ 5. Back on your Heroku app page, go to the _Settings_ tab.
+ 6. Under the _Config Variables_ section, click the _Reveal Config Vars_ button.
+ 7. Enter `MATTERMOST_WEBHOOK_URL` for the _KEY_ and paste the URL you copied as the _VALUE_, then click _Add_.
+
+That's it! The integration should now be up and running on Heroku. It might take a minute for the Heroku process to finish starting but after that try performing an action on your GitLab project to trigger a post in Mattermost.
+
+### Manual Install
+
+You of course don't have to use Heroku if you don't want to, you can easily set up the integration to run on practically any web server. It can even go behind your firewall as long as the integration still has access to your GitLab and Mattermost instances.
+
+Below are the loose instructions for setting up the integration on a Linux/Ubuntu server.
+
+3. Set up your Mattermost incoming webhook
+ 1. Log in to your Mattermost account, and open your _Account Settings_ by clicking in the top-left.
+ 2. Go to the _Integrations_ tab and click _Edit_ next to _Manage your incoming webhooks_.
+ 3. Select the channel you want the GitLab events to post to, then click _Add_.
+ 4. Copy the _URL_ from the newly created webhook and keep it handy for the next steps.
+
+1. Set up your server
+ 1. Stand-up a Linux/Ubuntu server on AWS, your own machine or somewhere else.
+ 1. SSH into the machine, or just open your terminal if you're installing locally.
+ 1. Make sure you have Python 2.7+ installed. If it's not installed you can find it [here](https://www.python.org/downloads/)
+    - `python --version`
+ 2. Install pip and other essentials
+    - `sudo apt-get install python-pip python-dev build-essential`
+ 3. Clone the repo with
+    - `git clone https://github.com/mattermost/gitlab-mattermost.git`
+    - `cd gitlab-mattermost`
+ 3. Install integration requirements
+    - `sudo pip -r requirements.txt`
+ 4. Add the following lines to your `~/.bash_profile`
+    - `export MATTERMOST_WEBHOOK_URL=<your-webhook-url>` This is the URL you copied in the last section
+    - `export PORT=<your-port-number>` The port number you want the integration to listen on (defaults to 5000)
+ 5. Source your bash profile
+    - `source ~/.bash_profile`
+ 5. Run the server
+    - `python server.py`
+
+2. Set up your GitLab outgoing webhook
+ 1. Log in to your GitLab account, and go to the project you want events pushed from.
+ 2. From your project page, click _Settings_ in the bottom-left and then click _Web Hooks_.
+ 3. In the _URL_ field, enter the following `<your-public-server-domain>/new_event`. Make sure to replace `<your-public-server-domain>` with the domain that translates to your public server IP address. Don't forget to include the port if needed. For example, `http://myserver.com:5000/new_event`
+ 4. Select all the _Triggers_ you want to be posted into Mattermost, then click _Add Web Hook_.
+
+That's it! The integration should now be running and ready to push GitLab events into Mattermost.
---
+++ server.py
@@ -0,0 +1,199 @@
+import os
+import sys
+import requests
+import json
+import time
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+USERNAME = 'gitlab'
+ICON_URL = 'https://gitlab.com/uploads/project/avatar/13083/gitlab-logo-square.png'
+MATTERMOST_WEBHOOK_URL = '' # Paste the Mattermost webhook URL you created here
+CHANNEL = '' # Leave this blank to post to the default channel of your webhook
+
+PUSH_EVENT = 'push'
+ISSUE_EVENT = 'issue'
+TAG_EVENT = 'tag_push'
+COMMENT_EVENT = 'note'
+MERGE_EVENT = 'merge_request'
+
+REPORT_EVENTS = {
+    PUSH_EVENT: False, # On pushes to the repository excluding tags
+    ISSUE_EVENT: True, # On creation of a new issue
+    TAG_EVENT: False, # On creation of tags
+    COMMENT_EVENT: True, # When a new comment is made on commits, merge requests, issues, and code snippets
+    MERGE_EVENT: True, # When a merge request is created
+}
+
+@app.route('/')
+def root():
+    """
+    Home handler
+    """
+
+    return "OK"
+
+@app.route('/new_event', methods=['POST'])
+def new_event():
+    """
+    GitLab event handler, handles POST events from a GitLab project
+    """
+
+    if request.json is None:
+        print 'Invalid Content-Type'
+        return 'Content-Type must be application/json and the request body must contain valid JSON', 400
+
+    data = request.json
+    object_kind = data['object_kind']
+
+    text = ''
+
+    if REPORT_EVENTS[PUSH_EVENT] and  object_kind == PUSH_EVENT:
+        text = '%s pushed %d commit(s) into the `%s` branch for project [%s](%s).' % (
+            data['user_name'],
+            data['total_commits_count'],
+            data['ref'],
+            data['repository']['name'],
+            data['repository']['homepage']
+        )
+    elif REPORT_EVENTS[ISSUE_EVENT] and object_kind == ISSUE_EVENT:
+        action = data['object_attributes']['action']
+
+        if action == 'open' or action == 'reopen':
+            description = add_markdown_quotes(data['object_attributes']['description'])
+
+            text = '#### [#%s - %s](%s)\n_[Issue](%s/issues) created by %s in [%s](%s) on %s_\n %s' % (
+                data['object_attributes']['iid'],
+                data['object_attributes']['title'],
+                data['object_attributes']['url'],
+                data['repository']['homepage'],
+                data['user']['username'],
+                data['repository']['name'],
+                data['repository']['homepage'],
+                data['object_attributes']['created_at'],
+                description
+            )
+    elif REPORT_EVENTS[TAG_EVENT] and object_kind == TAG_EVENT:
+        text = '%s pushed tag `%s` to the project [%s](%s).' % (
+            data['user_name'],
+            data['ref'],
+            data['repository']['name'],
+            data['repository']['homepage']
+        )
+    elif REPORT_EVENTS[COMMENT_EVENT] and object_kind == COMMENT_EVENT:
+        symbol = ''
+        type_grammar = 'a'
+        note_type = data['object_attributes']['noteable_type'].lower()
+        note_id = ''
+        parent_title = ''
+
+        if note_type == 'mergerequest':
+            symbol = '!'
+            note_id = data['merge_request']['iid']
+            parent_title = data['merge_request']['title']
+            note_type = 'merge request'
+        elif note_type == 'snippet':
+            symbol = '$'
+            note_id = data['snippet']['iid']
+            parent_title = data['snippet']['title']
+        elif note_type == 'issue':
+            symbol = '#'
+            note_id = data['issue']['iid']
+            parent_title = data['issue']['title']
+            type_grammar = 'an'
+
+        subtitle = ''
+        if note_type == 'commit':
+            subtitle = '%s' % data['commit']['id']
+        else:
+            subtitle = '%s%s - %s' % (symbol, note_id, parent_title)
+
+        description = add_markdown_quotes(data['object_attributes']['note'])
+
+        text = '#### **New Comment** on [%s](%s)\n_[%s](https://gitlab.com/u/%s) commented on %s %s in [%s](%s) on %s_\n %s' % (
+            subtitle,
+            data['object_attributes']['url'],
+            data['user']['username'],
+            data['user']['username'],
+            type_grammar,
+            note_type,
+            data['repository']['name'],
+            data['repository']['homepage'],
+            data['object_attributes']['created_at'],
+            description
+        )
+    elif REPORT_EVENTS[MERGE_EVENT] and object_kind == MERGE_EVENT:
+        action = data['object_attributes']['action']
+
+        if action == 'open' or action == 'reopen':
+            description = add_markdown_quotes(data['object_attributes']['description'])
+
+            text = '#### [!%s - %s](%s)\n*[%s](https://gitlab.com/u/%s) created a merge request in [%s](%s) on %s*\n %s' % (
+                data['object_attributes']['iid'],
+                data['object_attributes']['title'],
+                data['object_attributes']['url'],
+                data['user']['username'],
+                data['user']['username'],
+                data['object_attributes']['target']['name'],
+                data['object_attributes']['target']['web_url'],
+                data['object_attributes']['created_at'],
+                description
+            )
+
+    if len(text) == 0:
+        print 'Text was empty so nothing sent to Mattermost, object_kind=%s' % object_kind
+        return 'OK'
+
+    post_text(text)
+
+    return 'OK'
+
+def post_text(text):
+    """
+    Mattermost POST method, posts text to the Mattermost incoming webhook URL
+    """
+
+    data = {}
+    data['text'] = text
+    if len(USERNAME) > 0:
+        data['username'] = USERNAME
+    if len(ICON_URL) > 0:
+        data['icon_url'] = ICON_URL
+    if len(CHANNEL) > 0:
+        data['channel'] = CHANNEL
+
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(MATTERMOST_WEBHOOK_URL, headers=headers, data=json.dumps(data))
+
+    if r.status_code is not requests.codes.ok:
+        print 'Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (MATTERMOST_WEBHOOK_URL, r.status_code, r.json)
+
+def add_markdown_quotes(text):
+    """
+    Add Markdown quotes around a piece of text
+    """
+
+    if len(text) == 0:
+        return ''
+
+    split_desc = text.split('\n')
+
+    for index, line in enumerate(split_desc):
+        split_desc[index] = '> ' + line
+
+    return '\n'.join(split_desc)
+
+if __name__ == "__main__":
+    MATTERMOST_WEBHOOK_URL = os.environ.get('MATTERMOST_WEBHOOK_URL', '')
+    CHANNEL = os.environ.get('CHANNEL', CHANNEL)
+    USERNAME = os.environ.get('USERNAME', USERNAME)
+    ICON_URL = os.environ.get('ICON_URL', ICON_URL)
+
+    if len(MATTERMOST_WEBHOOK_URL) == 0:
+        print 'MATTERMOST_WEBHOOK_URL must be configured. Please see instructions in README.md'
+        sys.exit()
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
